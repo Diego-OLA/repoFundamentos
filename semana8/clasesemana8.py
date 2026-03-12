f63bfd98782e5921d21306a0897f59f86c710acc
Diego <rlandaverde119@gmail.com>
2026-03-12 17:52:55 -0600

    funcion para cambiar texto a mayusculas y funcion para cambiar a minusculas

---
+++ semana8/clasesemana8.py
@@ -0,0 +1,49 @@
+# las comillas triples son las que se encargan de hacer
+# Cadenas de texto largas sin mdoficar el formato.
+# texto corto
+poema = (" Es porque un pajarito de la montaña ha hecho",)
+"en el hueco de un árbol, su nido matinal"
+"que el árbol amanece con música en el pecho",
+"como que si tuviera corazón musical"
+
+
+# textos largos ''' o """
+poema = """Es porque un pajarito de la montaña ha hecho,
+en el hueco de un árbol, su nido matinal,
+que el árbol amanece con música en el pecho,
+como que si tuviera corazón musical.
+
+Si el dulce pajarito por entre el hueco asoma,
+para beber rocío, para beber aroma,
+el árbol de la sierra me da la sensación
+de que se le ha salido, cantando, el corazón. """
+
+#print(poema)
+
+## computadora -> que variable queres imprimir
+## print()
+
+##MAYUSCULAS
+##Mutabilidad -> siempre debemos evitar transformar objeto original
+##clases -> estereotipo (como un molde)
+##propiedades ->
+##color 
+##tipo de motor (Electrico o gas)
+
+#ojos
+#color de pelo
+
+#funciones
+#moverse
+#frenar
+#cargarse
+#descargarse
+
+poema_Mayusculas = poema.upper()
+print(poema_Mayusculas)
+#convertir en minusculas
+#string .lower
+poema_minisculas = poema.lower()
+print(poema_minisculas)
+
+
