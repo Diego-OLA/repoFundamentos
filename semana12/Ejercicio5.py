f9d321165cf93a607911c594fbee11ae9d82e75f
Diego <rlandaverde119@gmail.com>
2026-04-17 10:32:51 -0600

    Ejercicio5

---
+++ semana12/Ejercicio5.py
@@ -0,0 +1,18 @@
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+operacion = input("Ingresa la operación (+, -, *, /): ")
+
+if operacion == "+":
+    print("Resultado:", num1 + num2)
+elif operacion == "-":
+    print("Resultado:", num1 - num2)
+elif operacion == "*":
+    print("Resultado:", num1 * num2)
+elif operacion == "/":
+    if num2 != 0:
+        print("Resultado:", num1 / num2)
+    else:
+        print("No se puede dividir entre cero")
+else:
+    print("Operación no válida")
